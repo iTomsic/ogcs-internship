25ec11e820ca23be43dc1638ba869511cf0c65fe
ptomsic <patriktomsic2@gmail.com>
2026-01-23 11:31:54 +0100

    Remove hello world Mapping and Rest Controller annotation

--- ticket-system/src/main/java/com/ogcs/ticketsystem/TicketSystemApplication.java
+++ ticket-system/src/main/java/com/ogcs/ticketsystem/TicketSystemApplication.java
@@ -6,16 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TicketSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketSystemApplication.class, args);
 	}
-
-	@GetMapping
-	public String helloWorld(){
-		return "Hello World!";
-	}
-
+	
 }
